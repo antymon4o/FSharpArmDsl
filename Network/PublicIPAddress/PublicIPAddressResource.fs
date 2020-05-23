1f43e281f18e343f028554a1d1aacce844d480f1
Anton Dimitrov <antymon@hotmail.com>
2020-05-23 12:23:53 +0300

    addded public ip address resource

---
+++ Network/PublicIPAddress/PublicIPAddressResource.fs
@@ -0,0 +1,47 @@
+﻿module NetworkPublicIPAddressResource
+
+open NetworkCommon
+
+type PublicIPAddress = {
+    name: string
+    ``type``: string
+    apiVersion: string
+    location: string
+    tags: Map<string, string> option
+    sku: PublicIPAddressSku option
+    properties: PublicIPAddressProperties
+    zones: string array option
+}
+and PublicIPAddressSku = {
+    name: PublicIPAddressSkuName
+}
+and PublicIPAddressSkuName = Basic | Standard
+and PublicIPAddressProperties = {
+    publicIPAllocationMethod: PublicIPAllocationMethod
+    publicIPAddressVersion: PublicIPAddressVersion
+    dnsSettings: PublicIPAddressDnsSettings option
+    ddosSettings: DdosSettings option
+    ipTags: IpTag array option
+    ipAddress: string option
+    publicIPPrefix: SubResource option
+    idleTimeoutInMinutes: int option
+
+}
+and PublicIPAllocationMethod = Static | NoDynamicInvocationAttribute
+and PublicIPAddressVersion = IPv4 | IPv6
+and PublicIPAddressDnsSettings = {
+    domainNameLabel: string option
+    fqdn: string option
+    reverseFqdn: string option
+
+}
+and DdosSettings  = {
+    ddosCustomPolicy: SubResource option
+    protectionCoverage: ProtectionCoverage option
+    protectedIP: bool option
+}
+and ProtectionCoverage = Basic | Standard
+and IpTag = {
+    ipTagType: string option
+    tag: string option
+}
